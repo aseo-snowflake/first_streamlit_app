6f91db474716d317360bfc0cc11ce1531f318b03
aseo-snowflake <132401157+aseo-snowflake@users.noreply.github.com>
2023-05-03 13:53:10 -0500

    Update streamlit_app.py
    header and button text changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
+
 # Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -50,7 +51,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
@@ -68,6 +69,3 @@ if streamlit.button('Add a Fruit to the List'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   my_cnx.close()
   streamlit.text(back_from_function)
-
-
-
