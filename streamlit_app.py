92554f694d746359cf3ef3ca9fde53d474010068
aseo-snowflake <132401157+aseo-snowflake@users.noreply.github.com>
2023-05-03 13:23:19 -0500

    Update streamlit_app.py
    Remove comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.title('My Parents New Healthy Diner')
-
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -27,7 +26,6 @@ streamlit.dataframe(fruits_to_show)
 # create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruity_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  # take the json version of the response and normalize it
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
@@ -39,7 +37,6 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
-    # output it to the screen as a table
     streamlit.dataframe(back_from_function)
 
 except URLError as e:
