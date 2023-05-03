634eb537622081d8d1987fa6addbede62992c5be
aseo-snowflake <132401157+aseo-snowflake@users.noreply.github.com>
2023-05-03 13:56:58 -0500

    Update streamlit_app.py
    Moved my_cnx line

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 # Add a button to add a new fruit to the list
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_cnx.close()
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
