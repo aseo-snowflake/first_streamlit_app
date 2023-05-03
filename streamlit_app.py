fab3d53fd6bb412a90f25d9cf39b624f464688a7
aseo-snowflake <132401157+aseo-snowflake@users.noreply.github.com>
2023-05-03 13:55:56 -0500

    Update streamlit_app.py
    Tried moving the my_cnx line

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,8 +64,10 @@ def insert_row_snowflake(new_fruit):
     return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
+# Add a button to add a new fruit to the list
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_function = insert_row_snowflake(add_my_fruit)
   my_cnx.close()
+  back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
